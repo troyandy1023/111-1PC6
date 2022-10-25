72c7ce89422675e811fd81e7a37caabf91985b1a
Toodle <alicebobo223@gmai.com>
2022-10-26 00:15:24 +0800

    起司起司烤了會牽絲 StinkyCheese不管配著什麼都好好吃StinkyCheese 焗烤披薩點心我都全部加起司 StinkyCheese 只要撒滿起司我全部都會吃 StinkyCheese 不過最重要的是 StinkyCheese 披薩邊邊沒起司 StinkyCheese 我就一口一口一個一個 StinkyCheese 全部丟到 坐在旁邊甚麼都吃 StinkyCheese 爸爸他的盤子 StinkyCheese 起司起司我最喜歡吃 StinkyCheese

---
+++ 111-1PC6/Test.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC6
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] school = new string[3] { "台科", "北科", "亞東" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for (int ct = 0; ct < school.Length; ct++)
+            {
+                ListItem ol = new ListItem();
+                ol.Text = school[ct];
+                ol.Value = school[ct];
+                ddl_Test.Items.Add(ol);
+            }
+        }
+    }
+}
